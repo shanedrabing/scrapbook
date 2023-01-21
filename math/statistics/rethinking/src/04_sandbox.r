4835cfdb657829bebeab87a9374ccb78f40b3dc1
Shane Drabing <shane.drabing@gmail.com>
2023-01-20 19:48:53 -0700

    BORED

--- math/statistics/rethinking/src/04_sandbox.r
+++ math/statistics/rethinking/src/04_sandbox.r
@@ -18,12 +18,36 @@ howell <- Howell1 %>%
     as_tibble()
 
 
+# DAG
+
+
+# H -> W
+# S -> W
+# S -> H
+
+
+# Sythesizing People
+
+
+# 1 is F
+# 2 is M
+sim_HW <- function(S, b, a) {
+    N <- length(S)
+    H <- ifelse(S == 1, 150, 160) + rnorm(N, 0, 5)
+    W <- a[S] + b[S] * H + rnorm(N, 0, 5)
+    data.frame(S, H, W)
+}
+
+S <- rbern(100) + 1
+df <- sim_HW(S, c(0.5, 0.6), c(0, 0))
+
+
 # VISUALIZATION
 
 
 # looking at Howell data, height by weight with sexes
 with(howell, {
-    pdf(here("../data/pdf/howell_sex.pdf"), 9, 6)
+    pdf(here("../data/pdf/howell_sex_scatter.pdf"), 9, 6)
     op <- par(mar = c(5.5, 5, 3, 1.5))
 
     # parameters
@@ -45,6 +69,19 @@ with(howell, {
     # legend for sex
     legend(min(height), max(weight), c("Male", "Female"),
            pch = shape, col = color, bg = "white")
+
+    par(op)
+    dev.off()
+})
+
+# height differences between sexes
+with(subset(howell, age >= 20), {
+    pdf(here("../data/pdf/howell_sex_box.pdf"), 9, 6)
+    op <- par(mar = c(5.5, 5, 3, 1.5))
+
+    boxplot(height ~ male, horizontal = TRUE,
+            col = c(2, 4),
+            main = "Howell !Kung Demographics (over 20 y.o.)")
 
     par(op)
     dev.off()
